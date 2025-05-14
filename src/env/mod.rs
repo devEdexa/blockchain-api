aa6c6c1e58f09b3d02d4f75907921415a7080b6b
Aseem PM <aseempm@protonmail.com>
2025-05-14 11:00:25 +0530

    added edexa env and configured

--- src/env/mod.rs
+++ src/env/mod.rs
@@ -16,10 +16,10 @@ use {
     std::{collections::HashMap, fmt::Display},
 };
 pub use {
-    allnodes::*, arbitrum::*, aurora::*, base::*, binance::*, drpc::*, dune::*, getblock::*,
-    infura::*, mantle::*, monad::*, morph::*, near::*, odyssey::*, pokt::*, publicnode::*,
-    quicknode::*, server::*, solscan::*, syndica::*, unichain::*, wemix::*, zerion::*, zksync::*,
-    zora::*,
+    allnodes::*, arbitrum::*, aurora::*, base::*, binance::*, drpc::*, dune::*, edexa::*,
+    getblock::*, infura::*, mantle::*, monad::*, morph::*, near::*, odyssey::*, pokt::*,
+    publicnode::*, quicknode::*, server::*, solscan::*, syndica::*, unichain::*, wemix::*,
+    zerion::*, zksync::*, zora::*,
 };
 mod allnodes;
 mod arbitrum;
@@ -28,6 +28,7 @@ mod base;
 mod binance;
 mod drpc;
 mod dune;
+mod edexa;
 mod getblock;
 mod infura;
 mod mantle;
